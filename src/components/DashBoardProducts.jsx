a12b3f3c0d8af380a034c7eb460945e4d1accf4e
adarsh sharma <37371998+adarshsharma27@users.noreply.github.com>
2024-04-14 00:02:41 +0530

    feat: Products Delete functionality Dashboard

--- src/components/DashBoardProducts.jsx
+++ src/components/DashBoardProducts.jsx
@@ -1,6 +1,42 @@
 import React from "react";
-
+import { LuTrash2, LuPencilLine } from "react-icons/lu";
+import conf, { databases } from "../conf/config";
+import toast from "react-hot-toast";
 const DashBoardProducts = ({ allProducts }) => {
+  const deleteProduct = async ($id) => {
+    try {
+      await databases.deleteDocument(conf.databaseId, conf.collectionId, $id);
+      toast.success("Product Deleted Successfully", {
+        duration: 4000,
+        position: "bottom-right",
+        style: {
+          background: "#fff",
+          color: "#252525",
+          padding: "20px",
+          fontWeight: "700",
+          boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.1)",
+          borderBottom: "3px solid #4F46E5",
+          borderRadius: "3px",
+          fontFamily: "Outfit, sans-serif",
+        },
+      });
+    } catch (error) {
+      toast.error(error.message, {
+        duration: 4000,
+        position: "top-right",
+        style: {
+          background: "#fff",
+          color: "#252525",
+          padding: "20px",
+          fontWeight: "700",
+          boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",
+          borderBottom: "3px solid #F17171",
+          borderRadius: "3px",
+          fontFamily: "Outfit, sans-serif",
+        },
+      });
+    }
+  };
   return (
     <>
       <div className="overflow-x-auto overflow-y-scroll h-[80vh] container my-6 mx-auto rounded-lg border border-gray-200  dark:bg-[#313E51] dark:shadow-2xl card-shadow-custom dark:text-white">
@@ -34,7 +70,10 @@ const DashBoardProducts = ({ allProducts }) => {
               <th className="whitespace-nowrap px-2 py-2  text-lg font-semibold text-gray-900 dark:text-white">
                 MainCategory
               </th>
-              <th className="whitespace-nowrap  px-2 py-2 text-lg font-semibold text-gray-900 dark:text-white">
+              <th
+                colspan="3"
+                className="whitespace-nowrap  px-2 py-2 text-lg font-semibold text-gray-900 dark:text-white"
+              >
                 SubCategory
               </th>
             </tr>
@@ -46,39 +85,53 @@ const DashBoardProducts = ({ allProducts }) => {
                 className="even:bg-gray-50 dark:bg-slate-600 text-center"
                 key={elements.$id}
               >
-                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 dark:text-white">
+                <td className="whitespace-nowrap px-2 py-2 font-medium text-gray-900 dark:text-white">
                   <img
                     src={elements.image}
                     alt={elements.title}
                     className="h-40 w-full object-cover transition duration-500 group-hover:scale-105"
                   />
                 </td>
-                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 dark:text-white">
+                <td className="whitespace-nowrap  px-2 py-2 font-medium text-gray-900 dark:text-white">
                   {elements.title}
                 </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-white text-transform: capitalize">
+                <td className="whitespace-nowrap  px-2 py-2 text-gray-700 dark:text-white text-transform: capitalize">
                   {elements.price}
                 </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-white">
+                <td className="whitespace-nowrap  px-2 py-2 text-gray-700 dark:text-white">
                   {elements.oldPrice}
                 </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-white">
+                <td className="whitespace-nowrap  px-2 py-2 text-gray-700 dark:text-white">
                   {elements.price}
                 </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-white">
+                <td className="whitespace-nowrap  px-2 py-2 text-gray-700 dark:text-white">
                   {elements.quantity}
                 </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-white">
+                <td className="whitespace-nowrap  px-2 py-2 text-gray-700 dark:text-white">
                   {elements.outOfStock === true ? "Unavailable" : "Available"}
                 </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-white">
+                <td className="whitespace-nowrap  px-2 py-2 text-gray-700 dark:text-white">
                   {elements.category}
                 </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-white">
+                <td className="whitespace-nowrap  px-2 py-2 text-gray-700 dark:text-white">
                   {elements.mainCategory}
                 </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-white">
+                <td className="whitespace-nowrap  px-2 py-2 text-gray-700 dark:text-white">
                   {elements.subCategory}
+                </td>
+                <td className="whitespace-nowrap px-2 py-2 text-gray-700 dark:text-white">
+                  <LuPencilLine
+                    className="hover:text-indigo-400 hover:cursor-pointer dark:text-white"
+                    size={30}
+                    onClick={() => {}}
+                  />
+                </td>
+                <td className="whitespace-nowrap px-2 py-2 text-gray-700 dark:text-white">
+                  <LuTrash2
+                    className="hover:text-red-400 hover:cursor-pointer dark:text-white"
+                    size={30}
+                    onClick={() => deleteProduct(elements.$id)}
+                  />
                 </td>
               </tr>
             ))}
